8352be4cdea224eda34a053ba1aa5f353e187744
Elvis Josué <68576522+iElvisJosue@users.noreply.github.com>
2023-05-21 23:15:06 -0600

    Add files via upload

--- js/email.js
+++ js/email.js
@@ -1,21 +1,35 @@
 const MailForm = document.getElementById('MailForm');
 const BtnSubmit = document.getElementById('BtnSubmit');
+const BtnSendLoad = document.querySelector('.Button-Send-Load');
+const IconSendLoad = document.querySelector('.Icon-Send-Load');
 const AlertSuccess = document.querySelector(".Alert-Success");
 const AlertError = document.querySelector(".Alert-Error");
 
 MailForm.addEventListener('submit', function(event) {event.preventDefault();
+
+    BtnSendLoad.innerHTML = "SENDING";
+    IconSendLoad.innerHTML = "refresh";
+    IconSendLoad.classList.add("Active");
+
     const serviceID = 'default_service';
     const templateID = 'template_it46bfn';
 
     emailjs.sendForm(serviceID, templateID, this)
         .then(() => {
             AlertSuccess.classList.add("Active");
+            IconSendLoad.innerHTML = "arrow_outward";
+            IconSendLoad.classList.remove("Active");
+            BtnSendLoad.innerHTML = "SEND EMAIL";
+            MailForm.reset();
             setTimeout(() => {
                 AlertSuccess.classList.remove("Active");
-                MailForm.reset();
             },5000)
         }, (err) => {
             AlertError.classList.add("Active");
+            IconSendLoad.innerHTML = "arrow_outward";
+            IconSendLoad.classList.remove("Active");
+            BtnSendLoad.innerHTML = "SEND EMAIL";
+            MailForm.reset();
             setTimeout(() => {
                 AlertError.classList.remove("Active");
             },5000)
--- js/theme.js
+++ js/theme.js
@@ -14,42 +14,50 @@ CloseMenuTheme.addEventListener("click", () => {
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[0].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#F45050');
+    VarCSS.style.setProperty('--MainColor', '#667eea');
+    VarCSS.style.setProperty('--SecundaryColor', '#764ba2');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[1].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#19A7CE');
+    VarCSS.style.setProperty('--MainColor', '#b062d2');
+    VarCSS.style.setProperty('--SecundaryColor', '#ee609c');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[2].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#806F21');
+    VarCSS.style.setProperty('--MainColor', '#8baaaa');
+    VarCSS.style.setProperty('--SecundaryColor', '#ae8b9c');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[3].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#654E92');
+    VarCSS.style.setProperty('--MainColor', '#f43b47');
+    VarCSS.style.setProperty('--SecundaryColor', '#453a94');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[4].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#C25797');
+    VarCSS.style.setProperty('--MainColor', '#f83600');
+    VarCSS.style.setProperty('--SecundaryColor', '#ec8c69');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[5].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#EB6440');
+    VarCSS.style.setProperty('--MainColor', '#1f6a5a');
+    VarCSS.style.setProperty('--SecundaryColor', '#30785e');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[6].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#008000');
+    VarCSS.style.setProperty('--MainColor', '#4e36bd');
+    VarCSS.style.setProperty('--SecundaryColor', '#2768d5');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
 Theme[7].addEventListener("click", () => {
-    VarCSS.style.setProperty('--Orange', '#222222');
+    VarCSS.style.setProperty('--MainColor', '#23263a');
+    VarCSS.style.setProperty('--SecundaryColor', '#27555b');
     MenuDisplay.classList.remove("ShowDisplay");
     Bodi.classList.remove("Stop-Scrolling");
 })
